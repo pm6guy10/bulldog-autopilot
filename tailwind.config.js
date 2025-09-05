b4c6e33ad22c27ae223dda0c9b760a218a119279
Brandon Kapp <b.kapp1010@gmail.com>
2025-09-04 17:17:26 -0700

    fix(app)!: Final stand - full system rebuild

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,2 +1,2 @@
-﻿/** @type {import('tailwindcss').Config} */
-module.exports = { content: ['./app/**/*.{js,ts,jsx,tsx,mdx}'], theme: { extend: {}, }, plugins: [], }
+﻿/** @type {import("tailwindcss").Config} */
+module.exports = { content: ["./app/**/*.{js,ts,jsx,tsx,mdx}"], theme: { extend: {}, }, plugins: [], };
